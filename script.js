70ecf02139b421868ec4f4ea8167542a21fef663
Jennie Lust <94746740+JennieLust@users.noreply.github.com>
2022-08-07 15:37:00 +0200

    fix functionality

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 error msg replaces other when not matched
 add JS that turns error msg unhidden when invalid output*/
 
-
+/* init divs */
 const firstNameErr = document.querySelector(".err1")
 const firstNameInp = document.querySelector(".first-name")
 const btn = document.querySelector(".submit-button")
@@ -19,11 +19,11 @@ const phoneInp = document.querySelector(".phone")
 const password1Err = document.querySelector(".err5")
 const password1Inp = document.querySelector(".password1")
 
-const password2Err = document.querySelector(".err6")
 const password2Inp = document.querySelector(".password2")
 
-
-btn.addEventListener('click', () => {
+/* validation upon clicking submit */
+btn.addEventListener('click', () => {    
+    /* checks if valid, applies appr style */
     if (firstNameInp.checkValidity() === false) {
         firstNameErr.style.color = "red";
         firstNameInp.style.borderColor = "red";
@@ -42,6 +42,77 @@ btn.addEventListener('click', () => {
         lastNameInp.style.border = "2px solid green";
         lastNameInp.style.borderRadius = "3px";
     }
+    if (emailInp.checkValidity() === false) {
+        emailErr.style.color = "red";
+        emailInp.style.borderColor = "red";
+        emailInp.style.borderRadius = "4px";
+    } else {
+        emailErr.style.color = "#21241a";
+        emailInp.style.border = "2px solid green";
+        emailInp.style.borderRadius = "3px";
+    }
+    if (phoneInp.checkValidity() === false) {
+        phoneErr.style.color = "red";
+        phoneInp.style.borderColor = "red";
+        phoneInp.style.borderRadius = "4px";
+    } else {
+        phoneErr.style.color = "#21241a";
+        phoneInp.style.border = "2px solid green";
+        phoneInp.style.borderRadius = "3px";
+    }
+    if (password1Inp.checkValidity() === false) {
+        password1Err.style.color = "red";
+        password1Inp.style.borderColor = "red";
+        password1Inp.style.borderRadius = "4px";
+    } else {
+        password1Err.style.color = "#21241a";
+        password1Inp.style.border = "2px solid green";
+        password1Inp.style.borderRadius = "3px";
+    }
+    if (password1Inp.checkValidity() === false) {
+        password2Inp.style.borderColor = "red";
+        password2Inp.style.borderRadius = "4px";
+    } else {
+        password2Inp.style.border = "2px solid green";
+        password2Inp.style.borderRadius = "3px";
+    }
+    /* validates passwords against each other and applies validation and style*/
+    if (password1Inp.value !== password2Inp.value) {
+        password1Inp.setCustomValidity("Invalid field")
+        password2Inp.setCustomValidity("Invalid field")
+        password1Err.innerHTML = "Lösenord måste matcha";
+        password1Err.style.color = "red";
+        password1Inp.style.borderColor = "red";
+        password1Inp.style.borderRadius = "4px";
+        password2Inp.style.borderColor = "red";
+        password2Inp.style.borderRadius = "4px";
+    } else {
+        password1Err.style.color = "#21241a";
+        password1Inp.style.border = "2px solid green";
+        password1Inp.style.borderRadius = "3px";
+        password2Inp.style.border = "2px solid green";
+        password2Inp.style.borderRadius = "3px";
+    }
+
+    /* Check to make sure obl. fields have values */
+    if (firstNameInp.value.length == 0) {
+        firstNameInp.setCustomValidity("Invalid field");
+    } 
+    if (lastNameInp.value.length == 0) {
+        lastNameInp.setCustomValidity("Invalid field");
+    } 
+    if (emailInp.value.length == 0) {
+        emailInp.setCustomValidity("Invalid field");
+    } 
+    if (emailInp.value.length == 0) {
+        emailInp.setCustomValidity("Invalid field");
+    } 
+    if (password1Inp.value.length == 0) {
+        password1Inp.setCustomValidity("Invalid field");
+    } 
+    if (password2Inp.value.length == 0) {
+        password2Inp.setCustomValidity("Invalid field");
+    }
 
 })
 
@@ -50,6 +121,15 @@ document.querySelector(".form1").appendChild(firstNameInp)
 document.querySelector(".form1").appendChild(firstNameErr)
 document.querySelector(".form2").appendChild(lastNameInp)
 document.querySelector(".form2").appendChild(lastNameErr)
-document.querySelector(".form3").appendChild(email)
+
+document.querySelector(".form3").appendChild(emailInp)
+document.querySelector(".form3").appendChild(emailErr)
+
+document.querySelector(".form4").appendChild(phoneInp)
+document.querySelector(".form4").appendChild(phoneErr)
+document.querySelector(".form5").appendChild(password1Inp)
+document.querySelector(".form5").appendChild(password1Err)
+document.querySelector(".form6").appendChild(password2Inp)
+
 
 document.querySelector(".button-area").appendChild(btn)
